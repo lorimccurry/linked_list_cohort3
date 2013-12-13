2675db81eed594fc49a6e059dc60918d6100a531
Lori McCurry <lori.mccurry@gmail.com>
2013-12-13 14:26:48 -0600

    add accessor :new_item

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,8 @@
 class LinkedList
+  attr_accessor :new_item
 
   def intialize
-
+    #@count = 0 possibly might need this later
   end
 
 
@@ -9,7 +10,11 @@ class LinkedList
     if @new_item.nil?
       @new_item = LinkedListItem.new(payload)
     else
-      @new_item.next_list_item = LinkedListItem.new(payload)
+      current_item = @new_item
+      while !current_item.last?
+        current_item = current_item.next_list_item #cycle through to the end
+      end
+      current_item.next_list_item = LinkedListItem.new(payload)
     end
   end
 
@@ -21,5 +26,17 @@ class LinkedList
     end
     current_item.payload
   end
+
+
+  # def get(index)
+  #   # @head_node = @new_item #this will not change
+  #   current_item = @new_item
+  #   index.times do
+  #     # @temp_node = @head_item.next_list_item
+  #     current_item = current_item.next_list_item
+  #   end
+  #   # @temp_node.payload
+  #   current_item.payload
+  # end
 
 end
