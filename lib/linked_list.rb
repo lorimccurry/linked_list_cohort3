084fce3746b09c9e13332a4effea456a454aae77
Lori McCurry <lori.mccurry@gmail.com>
2013-12-12 20:30:47 -0600

    Add & get item; tests 12-15

---
+++ lib/linked_list.rb
@@ -0,0 +1,30 @@
+class LinkedList
+
+  def intialize
+
+  end
+
+
+  def add_item(payload)
+    if @new_item.nil?
+      @new_item = LinkedListItem.new(payload)
+    else
+      @new_item.next_list_item = LinkedListItem.new(payload)
+    end
+  end
+
+
+  def get(index)
+    current_item = @new_item
+
+    if index == 0
+      current_item.payload
+    else
+      index.times do
+        current_item = current_item.next_list_item
+      end
+      current_item.payload
+    end
+  end
+
+end
